3f6b3c1418486d2b0e28b0d730ae1b671fb3af11
Jonathan Quick <jmquicksubaru1@gmail.com>
2019-10-02 10:58:16 -0400

    Add project files.

---
+++ PayRoll/Data/PayRollContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PayRoll.Models;
+
+namespace PayRoll.Models
+{
+    public class PayRollContext : DbContext
+    {
+        public PayRollContext (DbContextOptions<PayRollContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employee { get; set; }
+    }
+}
---
+++ PayRoll/Models/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PayRoll.Models
+{
+    public class Employee
+    {
+        public int employeeId { get; set; }
+        [Required]
+        [Display(Name = "Employee Name")]
+        public string employeeName { get; set; }
+        [Required]
+        [Display(Name = "Pay")]
+        public float rateOfPay { get; set; }
+        [Required]
+        [Display(Name = "Hours Worked")]
+        public decimal hoursWorked { get; set; }
+        [Required]
+        [Display(Name = "Pay Week")]
+        [DataType(DataType.Date)]
+        public DateTime payWeek { get; set; }
+    }
+}
